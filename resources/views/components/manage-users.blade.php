9e97af83ed7d1bab839e8e071f034cb820fe835a
ThunderZeus1 <thunder.zeus0908@gmail.com>
2024-06-16 01:47:08 +0530

    third Commit

---
+++ resources/views/components/manage-users.blade.php
@@ -0,0 +1,8 @@
+<!-- resources/views/components/manage-users.blade.php -->
+
+<div class="container mx-auto p-6">
+    <div class="bg-white text-gray-800 rounded-lg shadow-md p-6">
+        <h3 class="text-lg font-semibold mb-4">User Management</h3>
+        <p>This is an empty page for managing users. Add your content here.</p>
+    </div>
+</div>
